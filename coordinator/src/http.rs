7d4d042ee01d51e82d0092b98efe43f35c52fbe6
Samuel Maier <samuel.maier2@hotmail.de>
2025-06-04 17:20:42 +0200

    only send changes per websocket instead of a regular update (which it did so far, unintentionally) -- this currently causes UI issues!

--- coordinator/src/http.rs
+++ coordinator/src/http.rs
@@ -8,7 +8,7 @@ use std::{net::IpAddr, time::Duration};
 use std::{net::SocketAddr, sync::Arc};
 use tokio::net::TcpStream;
 use tokio::time::timeout;
-use tracing::{debug, info};
+use tracing::{debug, error, info, warn};
 
 use crate::{
     config::{ControllerConfig, load_coordinator_config, watch_config_file},
@@ -66,9 +66,29 @@ pub async fn start_http_server(config_path: &std::path::Path) {
     {
         let config_rx = config_rx.clone();
         let is_on_tx = is_on_tx.clone();
-        let ws_tx = ws_tx.clone();
         tokio::spawn(async move {
-            poll_host_statuses(config_rx, is_on_tx, ws_tx).await;
+            poll_host_statuses(config_rx, is_on_tx).await;
+        });
+    }
+    {
+        let ws_tx = ws_tx.clone();
+        let mut is_on_rx = is_on_rx.clone();
+        tokio::spawn(async move {
+            {
+                let initial_status = is_on_tx.borrow().clone();
+                let _ = ws_tx.send(serde_json::to_string(initial_status.as_ref()).unwrap());
+            }
+            loop {
+                if let Ok(_) = is_on_rx.changed().await {
+                    if let Ok(status_map) = serde_json::to_string(is_on_rx.borrow().as_ref()) {
+                        let _ = ws_tx.send(status_map);
+                    } else {
+                        error!("Failed to serialize status map");
+                    }
+                } else {
+                    warn!("Failed to receive change notification from is_on_tx");
+                }
+            }
         });
     }
 
@@ -114,7 +134,6 @@ pub async fn start_http_server(config_path: &std::path::Path) {
 async fn poll_host_statuses(
     config_rx: watch::Receiver<Arc<ControllerConfig>>,
     is_on_tx: watch::Sender<Arc<HashMap<String, bool>>>,
-    ws_tx: broadcast::Sender<String>,
 ) {
     loop {
         let config = config_rx.borrow().clone();
@@ -137,9 +156,17 @@ async fn poll_host_statuses(
             .into_iter()
             .collect();
 
-        // TODO: sends updates all the time to websocket clients, even if nothing changed.
-        let _ = ws_tx.send(serde_json::to_string(&status_map).unwrap());
-        let _ = is_on_tx.send(Arc::new(status_map));
+        let is_new = {
+            let old_status_map = is_on_tx.borrow();
+            let old_status_map = old_status_map.as_ref();
+            status_map != *old_status_map
+        };
+        if is_new {
+            info!("Host status changed: {:?}", status_map);
+            is_on_tx.send(Arc::new(status_map)).unwrap();
+        } else {
+            debug!("No change in host status");
+        }
 
         tokio::time::sleep(Duration::from_secs(2)).await;
     }
