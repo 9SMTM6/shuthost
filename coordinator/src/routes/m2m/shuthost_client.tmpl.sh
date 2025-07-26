384f9a108151a391932d8510993fa34396c3da64
Samuel Maier <samuel.maier2@hotmail.de>
2025-07-26 18:48:51 +0200

    add untested async option to client script

--- coordinator/src/routes/m2m/shuthost_client.tmpl.sh
+++ coordinator/src/routes/m2m/shuthost_client.tmpl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Usage: ./shuthost_client_<client_id>.sh <take|release> <host> [remote_url]
+# Usage: ./shuthost_client_<client_id>.sh <take|release> <host> [remote_url] [--async]
 # Requires: curl, openssl, date
 
 set -eu
@@ -8,7 +8,24 @@ set -eu
 ACTION="$1"
 TARGET_HOST="$2"
 REMOTE_URL="${3:-"{embedded_remote_url}"}"
+
+# TODO: test this option
+# Check for --async flag in remaining arguments
+ASYNC_MODE=false
+shift 3 2>/dev/null || shift $# # Remove first 3 args, or all if less than 3
+for arg in "$@"; do
+    case "$arg" in
+        --async)
+            ASYNC_MODE=true
+            ;;
+    esac
+done
+
+# Build coordinator URL with optional async parameter
 COORDINATOR_URL="${REMOTE_URL}/api/m2m/lease/${TARGET_HOST}/${ACTION}"
+if [ "$ASYNC_MODE" = true ]; then
+    COORDINATOR_URL="${COORDINATOR_URL}?async=true"
+fi
 
 CLIENT_ID="{client_id}"
 SECRET="{shared_secret}"
