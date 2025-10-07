83415dea382da4a7e713e087e9948ef2a2529b07
Samuel Maier <samuel.maier2@hotmail.de>
2025-10-07 11:55:27 +0200

    add mostly failing accessibility tests

---
+++ frontend-tests/tests/accessibility-scan.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright'; // 1
+import { startBackend, stopBackend, configs, expand_and_sanitize_host_install } from './test-utils';
+
+let backendProcess: any | undefined;
+
+test.beforeAll(async () => {
+    backendProcess = await startBackend(configs["hosts-and-clients"]);
+});
+
+test.describe('main page(s)', () => {
+    test('hosts page should not have any automatically detectable accessibility issues', async ({ page }) => {
+        test.fixme(true, "These themes are about to change");
+
+        await expand_and_sanitize_host_install(page);
+
+        const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+        expect(accessibilityScanResults.violations).toEqual([]);
+    });
+
+    test('clients page should not have any automatically detectable accessibility issues', async ({ page }) => {
+        test.fixme(true, "These themes are about to change");
+
+        await page.goto('#clients');
+
+        const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+        expect(accessibilityScanResults.violations).toEqual([]);
+    });
+
+    test('docs page should not have any automatically detectable accessibility issues', async ({ page }, testInfo) => {
+        test.fixme(testInfo.project.name === 'Desktop Dark', "These themes are about to change");
+        await page.goto('#architecture');
+
+        const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+        expect(accessibilityScanResults.violations).toEqual([]);
+    });
+});
+
+test.afterAll(async () => {
+    stopBackend(backendProcess);
+    backendProcess = undefined;
+});
