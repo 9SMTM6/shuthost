67e32a26f827a436c87908231add31e85c08a726
Samuel Maier <samuel.maier2@hotmail.de>
2025-10-08 21:45:23 +0200

    potentially fix compilation issues

--- build.rs
+++ build.rs
@@ -1,14 +1,13 @@
 use std::process::{Command, exit};
-use std::path::PathBuf;
-use std::str::FromStr;
 
 fn main() {
-    let assets_dir = PathBuf::from_str("frontend/assets").unwrap();
-    println!("cargo::rerun-if-changed={}/styles.css", assets_dir.display());
-    println!("cargo::rerun-if-changed={}/app.ts", assets_dir.display());
-    println!("cargo::rerun-if-changed={}/index.tmpl.html", assets_dir.display());
-    println!("cargo::rerun-if-changed={}/login.tmpl.html", assets_dir.display());
-    println!("cargo::rerun-if-changed={}/partials", assets_dir.display());
+    let frontend_dir = "frontend";
+    let assets_dir = "frontend/assets";
+    println!("cargo::rerun-if-changed={}/styles.tailwind.css", assets_dir);
+    println!("cargo::rerun-if-changed={}/app.ts", assets_dir);
+    println!("cargo::rerun-if-changed={}/index.tmpl.html", assets_dir);
+    println!("cargo::rerun-if-changed={}/login.tmpl.html", assets_dir);
+    println!("cargo::rerun-if-changed={}/partials", assets_dir);
 
     // Check npm
     if Command::new("npm").arg("--version").output().is_err() {
@@ -16,14 +15,14 @@ fn main() {
         exit(1);
     }
 
-    let status = Command::new("npm").args(["ci", "--omit=dev"]).current_dir(&assets_dir).status();
+    let status = Command::new("npm").arg("ci").current_dir(frontend_dir).status();
     match status {
         Ok(s) if s.success() => {}
         Ok(_) => { eprintln!("npm ci failed."); exit(1); }
         Err(e) => { eprintln!("Failed to run npm ci: {e}"); exit(1); }
     }
 
-    let status = Command::new("npm").arg("run").arg("build").current_dir(&assets_dir).status();
+    let status = Command::new("npm").arg("run").arg("build").current_dir(frontend_dir).status();
     match status {
         Ok(s) if s.success() => {
             println!("npm build completed successfully.");
