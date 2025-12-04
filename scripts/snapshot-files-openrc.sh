d572957f9b96bce4dec1ac00803c72c8427b5fec
Samuel Maier <samuel.maier2@hotmail.de>
2025-12-04 21:19:46 +0100

    Add install file snapshot CI Job

--- scripts/snapshot-files-openrc.sh
+++ scripts/snapshot-files-openrc.sh
@@ -7,4 +7,4 @@ set -e
 podman build -t shuthost-builder -f scripts/Containerfile.build .
 podman run --rm -v "$(pwd)/target:/host-target" shuthost-builder sh -c "cp -r /src/target/* /host-target/"
 
-./scripts/snapshot-files-container.sh "docker.io/heywoodlh/openrc:latest" "apk update && apk add curl patch file" "./install-file-snapshots/openrc" "./target/x86_64-unknown-linux-musl/release/shuthost_coordinator" "openrc"
+./scripts/snapshot-files-container.sh "docker.io/heywoodlh/openrc:latest" "apk update && apk add curl patch file" "./install-file-snapshots/openrc" "./target/x86_64-unknown-linux-musl/release/shuthost_coordinator"
