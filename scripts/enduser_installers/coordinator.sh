15188f674d906208a74b8d60a8f431fb8bc59372
Samuel Maier <samuel.maier2@hotmail.de>
2026-01-10 19:07:16 +0100

    Update docs regarding coordinator installer script

--- scripts/enduser_installers/coordinator.sh
+++ scripts/enduser_installers/coordinator.sh
@@ -4,6 +4,8 @@ set -eu
 
 # Helper script to install the ShutHost coordinator binary
 
+# This embeds the script during the release process. 
+# That build script then gets released as an asset, with a tagged download URL.
 . ./scripts/helpers.sh
 
 cleanup() {
