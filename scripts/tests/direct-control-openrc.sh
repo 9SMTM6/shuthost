76c99150608e0ff28a2935c716624ad60e02e8b6
Samuel Maier <samuel.maier2@hotmail.de>
2026-01-10 17:37:32 +0100

    Add local testing for the direct_control script

---
+++ scripts/tests/direct-control-openrc.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Test direct control script generation and execution on OpenRC
+
+set -e
+
+. ./scripts/helpers.sh
+
+build_musl
+
+# Build the container
+docker build -f scripts/tests/Containerfile.alpine -t shuthost-test-alpine .
+
+# Run the test
+docker run --rm -t --privileged -v "$(pwd)":/repo shuthost-test-alpine /bin/sh -c "
+cd /repo
+./scripts/tests/install-and-run-direct-control.sh ./target/x86_64-unknown-linux-musl/debug/shuthost_host_agent root
+"
