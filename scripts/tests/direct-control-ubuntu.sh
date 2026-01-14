32c236be0e1f04e306990bb1d584ca84ecedc260
Samuel Maier <samuel.maier2@hotmail.de>
2026-01-14 19:22:07 +0100

    Some fixes and improvements, add service installation to coverage

--- scripts/tests/direct-control-ubuntu.sh
+++ scripts/tests/direct-control-ubuntu.sh
@@ -4,14 +4,18 @@
 
 set -e
 
+# Check if the cargo llvm-cov subcommand exists and set up the environment
+if cargo llvm-cov --help > /dev/null 2>&1; then
+    eval "$(cargo llvm-cov show-env --export-prefix --remap-path-prefix)"
+fi
+
 # Build the host_agent binary
-cargo build --bin shuthost_host_agent --target x86_64-unknown-linux-gnu
+cargo build --bin shuthost_host_agent
 
 # Build the container
 docker build -f scripts/tests/Containerfile.systemd -t shuthost-test-systemd .
 
 # Run the test
-docker run --rm -t --privileged -v "$(pwd)":/repo --env-file scripts/tests/coverage.env shuthost-test-systemd /bin/sh -c "
-cd /repo
-./scripts/tests/install-and-run-direct-control.sh ./target/x86_64-unknown-linux-gnu/debug/shuthost_host_agent
+docker run --rm -t --privileged -v "$(pwd)":/repo --workdir /repo --env-file scripts/tests/coverage.env shuthost-test-systemd /bin/sh -c "
+./scripts/tests/install-and-run-direct-control.sh ./target/debug/shuthost_host_agent
 "
--- scripts/tests/service-installation-systemd.sh
+++ scripts/tests/service-installation-systemd.sh
@@ -4,15 +4,29 @@
 
 set -e
 
+# Check if the cargo llvm-cov subcommand exists and set up the environment
+if cargo llvm-cov --help > /dev/null 2>&1; then
+    eval "$(cargo llvm-cov show-env --export-prefix --remap-path-prefix)"
+fi
+
 # Build the coordinator binary
-cargo build --release --bin shuthost_host_agent --target x86_64-unknown-linux-gnu
-cargo build --release --bin shuthost_coordinator --target x86_64-unknown-linux-gnu --features include_linux_x86_64_agent
+cargo build --bin shuthost_host_agent
+cp ./target/debug/shuthost_host_agent ./target/release/
+cp ./target/debug/shuthost_host_agent ./target/x86_64-unknown-linux-gnu/debug/
+# copy agent debug build to release path for inclusion in coordinator
+cp ./target/debug/shuthost_host_agent ./target/x86_64-unknown-linux-gnu/release/
+# also fake the musl agent, since some scripts only look for that...
+cp ./target/debug/shuthost_host_agent ./target/x86_64-unknown-linux-musl/release/            
+cargo build --bin shuthost_coordinator --features include_linux_x86_64_agent,include_linux_musl_x86_64_agent
+cp ./target/debug/shuthost_coordinator ./target/release/
+cp ./target/debug/shuthost_coordinator ./target/x86_64-unknown-linux-gnu/debug/
+# copy coordinator debug build to release path for other scripts that expect the binary there
+cp ./target/debug/shuthost_coordinator ./target/x86_64-unknown-linux-gnu/release/
 
 # Build the container
 docker build -f scripts/tests/Containerfile.systemd -t shuthost-test-systemd .
 
 # Run the test
-docker run --rm -t --privileged -v "$(pwd)":/repo --env-file scripts/tests/coverage.env shuthost-test-systemd /bin/sh -c "
-cd /repo
-./scripts/tests/coordinator_and_agent_service_installation.sh ./target/x86_64-unknown-linux-gnu/release/shuthost_coordinator
+docker run --rm -t --privileged -v "$(pwd)":/repo --workdir /repo --env-file scripts/tests/coverage.env shuthost-test-systemd /bin/sh -c "
+./scripts/tests/coordinator_and_agent_service_installation.sh ./target/debug/shuthost_coordinator
 "
