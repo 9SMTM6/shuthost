fad15684e0abd7e4bb54cf6852658741d7a9e22a
Samuel Maier <samuel.maier2@hotmail.de>
2026-02-28 21:07:51 +0100

    Add untested code - just read and accepted as looking reasonably - to listen for and apply startup broadcasts
    * Tests currently complain about port conflicts - IIRC on the UDP port, but run fine.
    * we need to add a bunch of tests for at least SOME of the functionality.
    * we should probably make the warning and generally the timings of event loops etc configurable
    * we still have todos from the branch this is based on

--- coordinator/src/demo.rs
+++ coordinator/src/demo.rs
@@ -55,6 +55,7 @@ pub(crate) async fn run_demo_service(port: u16, bind: &str, subpath: &str) {
         hoststatus_tx,
         ws_tx: broadcast::channel(1).0,
         leases: LeaseState::new(LeaseMapRaw::default()).0,
+        host_overrides: Arc::new(tokio::sync::RwLock::new(HashMap::new())),
         auth: Arc::new(
             auth::Runtime::from_config(&AuthConfig::default(), None)
                 .await
