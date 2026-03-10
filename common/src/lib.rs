9SMTM6/shuthost

commit 9357e70a21a17fa4ecc5d757e100a1983bb4cd9f

Switch to using git-version for embedded version

coordinator/Cargo.toml
@@ -65,6 +65,7 @@ clap = { workspace = true, features = ["env"] }
 cookie = "0.18"
 eyre.workspace = true
 futures.workspace = true
+git-version.workspace = true
 hex.workspace = true
 hmac.workspace = true
 hyper = "1.x"
@@ -129,13 +130,14 @@ workspace = true
 base64.workspace = true
 cargo-about = "0.8.4"
 eyre.workspace = true
+git-version.workspace = true
 handlebars = "6.3.2"
 hex.workspace = true
 krates = "0.20.0"
 regex.workspace = true
 resvg = "0.47"
-serde.workspace = true
 serde_json.workspace = true
+serde.workspace = true
 sha2.workspace = true
 spdx = { version = "0.13", features = ["text"] }
 # stuck at outdated because of resvg

coordinator/build/templates.rs
@@ -13,6 +13,8 @@ macro_rules! include_frontend_asset {
     };
 }
 
+pub const VERSION: &str = git_version::git_version!();
+
 trait TemplateExt {
     fn include_svgs(&self, svg_hashes: &HashMap<String, String>) -> String;
     fn include_png_icons(&self, icon_hashes: &HashMap<u32, String>) -> String;
@@ -58,7 +60,7 @@ impl<T: AsRef<str>> TemplateExt for T {
         let s = self.as_ref();
         s.replace("{ description }", env!("CARGO_PKG_DESCRIPTION"))
             .replace("{ repository }", env!("CARGO_PKG_REPOSITORY"))
-            .replace("{ version }", env!("CARGO_PKG_VERSION"))
+            .replace("{ version }", VERSION)
     }
 
     /// Note that this doesnt provide the needed JS to show the warnings, these are only in `app.ts`, as parts of the code are used there,

coordinator/src/cli.rs
@@ -7,12 +7,13 @@ use std::env;
 
 #[cfg(unix)]
 use crate::install;
+use crate::install::{BINARY_NAME, VERSION};
 use clap::{Parser, Subcommand, ValueEnum};
 
 /// Top-level command-line interface definition.
 #[derive(Debug, Parser)]
-#[command(name = env!("CARGO_PKG_NAME"))]
-#[command(version = env!("CARGO_PKG_VERSION"))]
+#[command(name = BINARY_NAME)]
+#[command(version = VERSION)]
 #[command(about = env!("CARGO_PKG_DESCRIPTION"))]
 pub struct Cli {
     #[command(subcommand)]

coordinator/src/install/mod.rs
@@ -13,6 +13,7 @@ use std::{
 
 use clap::Parser;
 use eyre::WrapErr as _;
+use git_version::git_version;
 use nix::unistd::User;
 
 mod migration;
@@ -28,7 +29,17 @@ const SERVICE_FILE_TEMPLATE: &str =
 #[cfg(target_os = "linux")]
 const OPENRC_FILE_TEMPLATE: &str = include_str!("openrc.shuthost_coordinator.tmpl.sh");
 
-const BINARY_NAME: &str = env!("CARGO_PKG_NAME");
+pub const BINARY_NAME: &str = env!("CARGO_PKG_NAME");
+pub const VERSION: &str = git_version!(
+    fallback = "unknown",
+    cargo_prefix = "cargo:",
+    args = [
+        "--tags",
+        "--exclude=nightly_release*",
+        "--always",
+        "--dirty=-modified"
+    ]
+);
 
 /// Arguments for the `install` subcommand of the coordinator.
 #[derive(Debug, Parser)]

coordinator/src/lib.rs
@@ -42,6 +42,8 @@ use demo::run_demo_service;
 
 use app::start;
 
+use crate::install::VERSION;
+
 static INIT_TRACING: Once = Once::new();
 static INIT_RUSTLS: Once = Once::new();
 
@@ -95,7 +97,7 @@ pub async fn inner_main(invocation: Cli) -> Result<()> {
 
             // Create a startup span that holds the resolved config path for the lifetime
             // of the coordinator initialization phase.
-            let startup_span = tracing::info_span!("coord.startup", ?config_path, pid=?process::id(), version = env!("CARGO_PKG_VERSION"));
+            let startup_span = tracing::info_span!("coord.startup", ?config_path, pid=?process::id(), version = VERSION);
             let _startup_enter = startup_span.enter();
 
             INIT_RUSTLS.call_once(|| {

host_agent/src/lib.rs
@@ -18,10 +18,14 @@ use clap::{Parser, Subcommand};
 
 use server::ServiceOptions;
 
+use crate::install::BINARY_NAME;
+
+pub const VERSION: &str = git_version::git_version!();
+
 /// Top-level CLI parser for `host_agent`.
 #[derive(Debug, Parser)]
-#[command(name = env!("CARGO_PKG_NAME"))]
-#[command(version = env!("CARGO_PKG_VERSION"))]
+#[command(name = BINARY_NAME)]
+#[command(version = VERSION)]
 #[command(author = env!("CARGO_PKG_AUTHORS"))]
 #[command(about = env!("CARGO_PKG_DESCRIPTION"))]
 pub struct Cli {

Cargo.toml
@@ -81,6 +81,7 @@ base64 = "0.22.1"
 clap = { version = "4.5.37", features = ["derive"] }
 eyre = "0.6.12"
 futures = "0.3.31"
+git-version = "0.3.9"
 hex = "0.4.3"
 hmac = "0.12.1"
 miniserde = "0.1"
@@ -99,6 +100,7 @@ shuthost_coordinator = { path = "./coordinator" }
 shuthost_host_agent = { path = "./host_agent" }
 tokio = { version = "1.44.2", features = ["full"] }
 toml = "1.x"
+
 
 [workspace.lints.rust]
 edition_2024_expr_fragment_specifier = "allow"

host_agent/Cargo.toml
@@ -13,9 +13,10 @@ categories.workspace = true
 [dependencies]
 base64.workspace = true
 clap.workspace = true
+git-version.workspace = true
+miniserde.workspace = true
 rand.workspace = true
 secrecy.workspace = true
-miniserde.workspace = true
 shuthost_common = { workspace = true, features = ["agent"] }
 
 [lints]

commit 7f73ca7564a2261d0b71d47e88ca60c04d7bdf38

Fix most (!) issues introduced by 9357e70a21a17fa4ecc5d757e100a1983bb4cd9f by unifying version definition to shuthost_common, and properly sanitizing version in frontend tests
* the remaining issue is regarding visual regression. The included version now changes on potentially a single whitespace change that git notices, thats not acceptable in snapshots.
    * But so far I've not sanitized the version on purpose, to have proper screenshots with the up-to-date version in the testing folder
    * I'll need to re-evaluate that approach

common/Cargo.toml
@@ -13,6 +13,7 @@ categories.workspace = true
 [dependencies]
 hex.workspace = true
 hmac.workspace = true
+git-version.workspace = true
 miniserde = { workspace = true, optional = true }
 nix.workspace = true
 secrecy.workspace = true

common/src/lib.rs
@@ -15,6 +15,7 @@ mod validation;
 
 use std::{net::UdpSocket, path};
 
+use git_version::git_version;
 pub use map_to_str::*;
 pub use protocol::*;
 pub use service_install::*;
@@ -34,6 +35,17 @@ pub const DEFAULT_COORDINATOR_BROADCAST_PORT: u16 = 5757;
 /// the various installer templates.  Again, installers must be manually updated
 /// if this value changes.
 pub const DEFAULT_AGENT_TCP_PORT: u16 = 9090;
+
+pub const VERSION: &str = git_version!(
+    fallback = "unknown",
+    cargo_prefix = "cargo:",
+    args = [
+        "--tags",
+        "--exclude=nightly_release*",
+        "--always",
+        "--dirty=-modified"
+    ]
+);
 
 /// Creates a UDP socket configured for broadcasting on the specified port.
 ///

coordinator/Cargo.toml
@@ -130,7 +130,6 @@ workspace = true
 base64.workspace = true
 cargo-about = "0.8.4"
 eyre.workspace = true
-git-version.workspace = true
 handlebars = "6.3.2"
 hex.workspace = true
 krates = "0.20.0"
@@ -139,6 +138,7 @@ resvg = "0.47"
 serde_json.workspace = true
 serde.workspace = true
 sha2.workspace = true
+shuthost_common = { workspace = true, features = ["coordinator"] }
 spdx = { version = "0.13", features = ["text"] }
 # stuck at outdated because of resvg
 tiny-skia = "0.12"

coordinator/build/templates.rs
@@ -1,6 +1,7 @@
 use base64::{Engine as _, engine::general_purpose};
 use eyre::WrapErr as _;
 use sha2::{Digest as _, Sha256};
+use shuthost_common::VERSION;
 use std::{
     collections::HashMap,
     fs,
@@ -12,8 +13,6 @@ macro_rules! include_frontend_asset {
         include_str!(concat!("../../frontend/assets/", $path))
     };
 }
-
-pub const VERSION: &str = git_version::git_version!();
 
 trait TemplateExt {
     fn include_svgs(&self, svg_hashes: &HashMap<String, String>) -> String;

coordinator/src/cli.rs
@@ -7,8 +7,9 @@ use std::env;
 
 #[cfg(unix)]
 use crate::install;
-use crate::install::{BINARY_NAME, VERSION};
+use crate::install::BINARY_NAME;
 use clap::{Parser, Subcommand, ValueEnum};
+use shuthost_common::VERSION;
 
 /// Top-level command-line interface definition.
 #[derive(Debug, Parser)]

coordinator/src/install/mod.rs
@@ -13,7 +13,6 @@ use std::{
 
 use clap::Parser;
 use eyre::WrapErr as _;
-use git_version::git_version;
 use nix::unistd::User;
 
 mod migration;
@@ -30,16 +29,6 @@ const SERVICE_FILE_TEMPLATE: &str =
 const OPENRC_FILE_TEMPLATE: &str = include_str!("openrc.shuthost_coordinator.tmpl.sh");
 
 pub const BINARY_NAME: &str = env!("CARGO_PKG_NAME");
-pub const VERSION: &str = git_version!(
-    fallback = "unknown",
-    cargo_prefix = "cargo:",
-    args = [
-        "--tags",
-        "--exclude=nightly_release*",
-        "--always",
-        "--dirty=-modified"
-    ]
-);
 
 /// Arguments for the `install` subcommand of the coordinator.
 #[derive(Debug, Parser)]

coordinator/src/lib.rs
@@ -24,6 +24,7 @@ pub mod wol;
 
 #[cfg(unix)]
 use nix::sys::stat;
+use shuthost_common::VERSION;
 use tracing::Instrument as _;
 // for use in integration tests
 pub use websocket::WsMessage;
@@ -41,8 +42,6 @@ use cli::{Cli, Command, LogFormat};
 use demo::run_demo_service;
 
 use app::start;
-
-use crate::install::VERSION;
 
 static INIT_TRACING: Once = Once::new();
 static INIT_RUSTLS: Once = Once::new();

host_agent/src/lib.rs
@@ -17,10 +17,9 @@ use std::env;
 use clap::{Parser, Subcommand};
 
 use server::ServiceOptions;
+use shuthost_common::VERSION;
 
 use crate::install::BINARY_NAME;
-
-pub const VERSION: &str = git_version::git_version!();
 
 /// Top-level CLI parser for `host_agent`.
 #[derive(Debug, Parser)]

frontend/tests/mobile-navigation.spec.ts
@@ -31,7 +31,7 @@ test.describe('mobile navigation', () => {
         await page.evaluate(() => {
             const footer = document.querySelector('footer');
             if (footer && footer.textContent) {
-                footer.textContent = footer.textContent.replace(/ShutHost Coordinator v[\d.]+/, 'ShutHost Coordinator v<<VERSION>>');
+                footer.textContent = footer.textContent.replace(/ShutHost Coordinator v[0-9A-Za-z.\-]+/, 'ShutHost Coordinator v<<VERSION>>');
             }
         });
 