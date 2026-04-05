00de3d56093dce740e477579ff84890886002023
Samuel Maier <samuel.maier2@hotmail.de>
2026-04-05 10:56:38 +0200

    fmt

--- tests/rs_integration/common.rs
+++ tests/rs_integration/common.rs
@@ -12,7 +12,8 @@
 
 use core::time::Duration;
 use std::{
-    env, fs, io::Write as _,
+    env, fs,
+    io::Write as _,
     net::{TcpListener, TcpStream as StdTcpStream},
     path::Path,
     thread,
