1e51516ea14a5f280907139a0edb5c1a34634777
Samuel Maier <samuel.maier2@hotmail.de>
2026-04-07 19:21:01 +0200

    bit more refactoring

--- frontend/playwright.live-demo.config.ts
+++ frontend/playwright.live-demo.config.ts
@@ -6,9 +6,11 @@ import { baseConfig } from './playwright.config';
 const DEMO_URL = process.env['DEMO_URL'] ?? 'https://9smtm6.github.io/shuthost';
 
 export default defineConfig({
-    ...baseConfig,
-    globalSetup: '',
-    globalTeardown: '',
+    expect: baseConfig.expect,
+    reporter: baseConfig.reporter,
+    timeout: baseConfig.timeout,
+    testDir: baseConfig.testDir,
+    fullyParallel: true,
     testMatch: ['live-demo-functional.spec.ts', 'live-demo-visual.spec.ts'],
     outputDir: '../target/playwright-live-demo-results/',
     use: {
