79412d4847fbaef2556ecd4899ee9f914bb0a571
Samuel Maier <samuel.maier2@hotmail.de>
2026-04-11 11:30:16 +0200

    unify and update migrations for easier understanding

--- coordinator/migrations/20260408180004_replace_push_subscription_host_online_with_unscheduled.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS push_subscription_host_online;
-
-CREATE TABLE push_subscription_host_unscheduled (
-    subscription_id INTEGER NOT NULL
-        REFERENCES push_subscriptions(id) ON DELETE CASCADE,
-    hostname        TEXT    NOT NULL,
-    PRIMARY KEY (subscription_id, hostname)
-);
