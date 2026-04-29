467eeb45fe8115168fc7f7781563b9b8498071f3
Samuel Maier <samuel.maier2@hotmail.de>
2026-04-29 11:21:00 +0200

    remove presumably unneeded steps that broke the CI

--- scripts/tests/run-update-alpine.sh
+++ scripts/tests/run-update-alpine.sh
@@ -14,16 +14,8 @@ fi
 . ./scripts/helpers.sh
 
 if [ -n "${1:-}" ]; then
-    directory="./target/x86_64-unknown-linux-musl/debug"
-    mkdir -p "$directory"
-    src="$1"
-    dest="$directory/shuthost_coordinator"
-    if [ "$src" != "$dest" ]; then
-        cp "$src" "$dest"
-    fi
-
-    mkdir -p ./target/x86_64-unknown-linux-musl/release
-    cp ./target/debug/shuthost_host_agent ./target/x86_64-unknown-linux-musl/release/shuthost_host_agent
+    mkdir -p ./target/x86_64-unknown-linux-musl/debug/
+    cp "$1" ./target/x86_64-unknown-linux-musl/debug/shuthost_coordinator
 else
     build_musl
 fi
